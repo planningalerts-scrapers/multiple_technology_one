be937b062b4061bb16904b47c099aa04c6222673
Matthew Landauer <mlandauer@gmail.com>
2021-05-12 13:05:57 +1000

    Burnside is now doing everything through the SA planning portal
    https://github.com/planningalerts-scrapers/issues/issues/283

--- lib/technology_one_scraper/authorities.rb
+++ lib/technology_one_scraper/authorities.rb
@@ -154,11 +154,6 @@ module TechnologyOneScraper
       # Mornington peninsula is blocking requests from outside Australia
       australian_proxy: true
     },
-    burnside: {
-      # url: "https://eservices.burnside.sa.gov.au/eProperty/P1/eTrack/eTrackApplicationSearch.aspx?r=P1.WEBGUEST&f=%24P1.ETR.SEARCH.ENQ"
-      url: "https://eservices.burnside.sa.gov.au/eProperty",
-      period: "L28"
-    },
     newcastle: {
         url: "https://cn-web.t1cloud.com/T1PRDefault/WebApps/eProperty",
         webguest: "TCON.LG.WEBGUEST",
